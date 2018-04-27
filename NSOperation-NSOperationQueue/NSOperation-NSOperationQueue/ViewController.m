7d8453dc072f088d26128fd64dc53258a85eefea
MK <mk@MacBook-Pro.local>
2018-04-27 16:09:29 +0800

    .

--- NSOperation-NSOperationQueue/NSOperation-NSOperationQueue/ViewController.m
+++ NSOperation-NSOperationQueue/NSOperation-NSOperationQueue/ViewController.m
@@ -9,9 +9,9 @@
 #import "ViewController.h"
 #import "MKOperation.h"
 @interface ViewController ()
-
+@property(nonatomic, weak)NSLock *lock;
+@property(nonatomic, assign) NSInteger ticketSurplusCount;
 @end
-
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -26,7 +26,64 @@
 //    [cusOperation start];
     
 //    [self addOperationWithBlockToQueue];
-    [self addOperationToQueue];
+//    [self addOperationToQueue];
+//    [self addDependency];
+    [self initTicketStatusSave];
+}
+/**
+ * 线程间通信
+ */
+- (void)communication {
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc]init];
+    // 2.添加操作
+    [queue addOperationWithBlock:^{
+        // 异步进行耗时操作
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+        
+        // 回到主线程
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // 进行一些 UI 刷新等操作
+            for (int i = 0; i < 2; i++) {
+                [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+                NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+            }
+        }];
+    }];
+}
+/**
+ * 操作依赖
+ * 使用方法：addDependency:
+ */
+- (void)addDependency {
+    
+    // 1.创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    // 2.创建操作
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"1---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
+    
+    // 3.添加依赖 (不可以相互依赖)
+    [op2 addDependency:op1]; // 让op2 依赖于 op1，则先执行op1，在执行op2
+    
+    // 4.添加操作到队列中
+    [queue addOperation:op1];
+    [queue addOperation:op2];
+    NSLog(@"completion");
 }
 /**
  * 使用 addOperation: 将操作加入到操作队列中
@@ -34,15 +91,18 @@
 - (void)addOperationToQueue {
     
     // 1.创建队列
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    
+    NSOperationQueue *queue = [NSOperationQueue mainQueue];
     // 2.创建操作
     // 使用 NSInvocationOperation 创建操作1
     NSInvocationOperation *op1 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task1) object:nil];
     
     // 使用 NSInvocationOperation 创建操作2
-    NSInvocationOperation *op2 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(task2) object:nil];
-    
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        for (int i = 0; i < 2; i++) {
+            [NSThread sleepForTimeInterval:2]; // 模拟耗时操作
+            NSLog(@"2---%@", [NSThread currentThread]); // 打印当前线程
+        }
+    }];
     // 使用 NSBlockOperation 创建操作3
     NSBlockOperation *op3 = [NSBlockOperation blockOperationWithBlock:^{
         for (int i = 0; i < 2; i++) {
@@ -61,6 +121,7 @@
     [queue addOperation:op1]; // [op1 start]
     [queue addOperation:op2]; // [op2 start]
     [queue addOperation:op3]; // [op3 start]
+    NSLog(@"completion");
 }
 /**
  * 使用 addOperationWithBlock: 将操作加入到操作队列中
@@ -69,6 +130,10 @@
 - (void)addOperationWithBlockToQueue {
     // 1.创建队列
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    // 2.设置最大并发操作数
+    queue.maxConcurrentOperationCount = 1; // 串行队列
+    // queue.maxConcurrentOperationCount = 2; // >1 并发队列
     
     // 2.使用 addOperationWithBlock: 添加操作到队列中
     [queue addOperationWithBlock:^{
@@ -116,5 +181,67 @@
     //    NSOperationQueue *queue = [NSOperationQueue mainQueue];
     //    queue.maxConcurrentOperationCount = 1;
     //    [queue addOperation:op];
+}
+
+/**
+ * 线程安全：使用 NSLock 加锁
+ * 初始化火车票数量、卖票窗口(线程安全)、并开始卖票
+ */
+
+- (void)initTicketStatusSave {
+    NSLog(@"currentThread---%@",[NSThread currentThread]); // 打印当前线程
+    
+    self.ticketSurplusCount = 50;
+    
+    NSLock *lock = [[NSLock alloc] init];  // 初始化 NSLock 对象
+    self.lock = lock;
+    // 1.创建 queue1,queue1 代表北京火车票售卖窗口
+    NSOperationQueue *queue1 = [[NSOperationQueue alloc] init];
+    queue1.maxConcurrentOperationCount = 1;
+    
+    // 2.创建 queue2,queue2 代表上海火车票售卖窗口
+    NSOperationQueue *queue2 = [[NSOperationQueue alloc] init];
+    queue2.maxConcurrentOperationCount = 1;
+    
+    // 3.创建卖票操作 op1
+    __weak typeof(self) weakSelf = self;
+    NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+    
+    // 4.创建卖票操作 op2
+    NSBlockOperation *op2 = [NSBlockOperation blockOperationWithBlock:^{
+        [weakSelf saleTicketSafe];
+    }];
+    
+    // 5.添加操作，开始卖票
+    [queue1 addOperation:op1];
+    [queue2 addOperation:op2];
+}
+
+/**
+ * 售卖火车票(线程安全)
+ */
+- (void)saleTicketSafe {
+    while (1) {
+        
+        // 加锁
+        [self.lock lock];
+        
+        if (self.ticketSurplusCount > 0) {
+            //如果还有票，继续售卖
+            self.ticketSurplusCount--;
+            NSLog(@"%@", [NSString stringWithFormat:@"剩余票数:%d 窗口:%@", self.ticketSurplusCount, [NSThread currentThread]]);
+            [NSThread sleepForTimeInterval:0.2];
+        }
+        
+        // 解锁
+        [self.lock unlock];
+        
+        if (self.ticketSurplusCount <= 0) {
+            NSLog(@"所有火车票均已售完");
+            break;
+        }
+    }
 }
 @end
